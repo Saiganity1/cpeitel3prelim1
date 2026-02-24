537e0dc879bdc6827f5c88f4567125666bb92b33
Saiganity1 <sicatmichaeldave0411@gmail.com>
2026-02-24 13:43:09 +0800

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="page">
+      <div className="layout">
+        <h1 className="student-name">Juan Dela Cruz</h1>
+
+        <div className="grid-container">
+          <div className="cell cell-1">1</div>
+          <div className="cell cell-2">2</div>
+          <div className="cell cell-3">3</div>
+          <div className="cell cell-4">4</div>
+          <div className="cell cell-5">5</div>
+          <div className="cell cell-6">6</div>
+          <div className="cell cell-7">7</div>
+          <div className="cell cell-8">8</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
